5754062cf65875da856d973da9769248d6c94fb2
Elhadji Malang DIEDHIOU <supermalang@outlook.com>
2017-11-08 17:59:49 +0000

    Corrections

--- SharePoint Online/EnableCustomScript.ps1
+++ SharePoint Online/EnableCustomScript.ps1
@@ -1,28 +1,24 @@
+
 <#
     Auteur : Elhadji Malang DIEDHIOU
     Description : Activation des scripts personnalisés sur les sites SharePoint Online
     Permissions : 
         - Autoriser les utilisateurs à exécuter des scripts personnalisés sur les sites personnels 
         - Autoriser les utilisateurs à exécuter des scripts personnalisés sur les sites créés en libre-service
+    Prerequis : Le module SharePoint Online Management Shell (https://www.microsoft.com/fr-fr/download/details.aspx?id=35588)
 #>
 
-# URL du site d'administration de SharePoint Online
-$adminUrl = Read-Host -Prompt "Veuillez entrer l'URL de votre site d'administration SharePoint Online"
-
-Write-Host "Appuyez sur une touche pour afficher la fenetre de connexion et renseigner vos identifiants..."
-$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+# nom du compte Office 365. Utilisé ici pour construire les URL
+$nomCompte = Read-Host -Prompt "Veuillez renseigner le nom de votre Office 365, exemple: hdlabs"
 
 # Identifiants administrateur
-$credentials = Get-Credential
+$adminCredentials = Get-Credential -Message "Veuillez renseigner vos identifiants d'administrateur Office 365"
 
 # Connexion au service SharePoint Online
-Connect-SPOService -Url $url -credential $credentials
-Write-Host "Connecte au service SharePoint avec succes"
+Connect-SPOService -Url https://$nomCompte-admin.sharepoint.com -Credential $adminCredentials
 
 # URL de la collection de site où on veut activer l'exécution des scripts personnalisés
 $siteURL = Read-Host -Prompt "Veuillez entrer l'URL de la collection de site ou vous voulez activer l'execution de scripts personnalises"
 
 # Autorisation de l'execution de scripts personnalisés
 Set-SPOSite -Identity $siteURL -DenyAddAndCustomizePages $false
-
-Write-Host "Exécution de scripts personnalises activee sur la collection de site $siteURL ."
